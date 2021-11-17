dcd0aea042526c7ae40fa89f8ae2191849ac1e27
michaelMatve <michael.matve@gmail.com>
2021-11-17 14:43:41 +0200

    with save and load csv

---
+++ src/allCalls.py
@@ -0,0 +1,34 @@
+import csv
+from callForElev import CallForElev
+class AllCalls:
+    def __init__(self):
+        self.calls = []
+        self.num_call=0
+
+    def loadFromcsv(self,file_name):
+        try:
+            with open(file_name) as file_csv:
+                csvreader=csv.reader(file_csv)
+                for v in csvreader:
+                    temp_call=CallForElev(time=v[1],src=v[2],dest=v[3],status=v[4],elev=v[5])
+                    self.addCall(temp_call)
+        except IOError as e:
+            print("csv fail")
+    def saveTocsv(self,file_name):
+        temp_arr= []
+        for x in self.calls:
+            temp_arr.append(x.__dict__.values())
+        try:
+            with open(file_name, 'w') as newCsv:
+                newWrite=csv.writer(newCsv)
+                newWrite.writerows(temp_arr)
+        except IOError as e:
+            print ("dont creat")
+
+    def addCall(self,tcall:CallForElev):
+        self.calls.append(tcall)
+        self.num_call+=1
+
+    def __str__(self):
+        return f"numofcall: {self.num_call}, calls: {self.calls}"
+
--- src/callForElev.py
+++ src/callForElev.py
@@ -1,1 +1,16 @@
-#class CallForElev:
+
+
+class CallForElev:
+    def __init__(self,time:float,src:int,dest:int,status:int,elev:int):
+        self.time=time
+        self.src=src
+        self.dest=dest
+        self.status=status
+        self.elev=elev
+
+    def __str__(self):
+        return f"time: {self.time}, src: {self.src}, dest: {self.dest}, status: {self.status}, elev: {self.elev} \n"
+
+    def __repr__(self):
+        return f"time: {self.time}, src: {self.src}, dest: {self.dest}, status: {self.status}, elev: {self.elev} \n"
+
