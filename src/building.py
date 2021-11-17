db6ae4c586c37e89217ec9c0f6e4274fc52b2886
michaelMatve <michael.matve@gmail.com>
2021-11-17 12:14:01 +0200

    try json

--- src/building.py
+++ src/building.py
@@ -1,6 +1,34 @@
+import json
+
+from elevator import Elevator
 class Building:
-    def __init__(self,minFloor:int,maxFloor:int):
+    def __init__(self,minFloor:int=0,maxFloor:int=0):
         self.elevators={}
         self.minFloor=minFloor
         self.maxFloor=maxFloor
+        self.numElev=0
+
+    def addElev(self,elev:Elevator):
+        self.elevators[elev.id]=elev
+        self.numElev+=1
+
+    def load_json(self,file_name):
+        try:
+            with open(file_name) as jasonFail:
+                self.elevators = {}
+                temp_dic=json.load(jasonFail)
+                self.minFloor=temp_dic["minFloor"]
+                self.maxFloor = temp_dic["maxFloor"]
+                temp_elev=temp_dic["elevators"]
+                for k,e in temp_elev.item():
+                    elev=Elevator(id=e["id"],speed=["speed"],minFloore=["minFloore"],maxFloor=e["maxFloor"],closeTime=e["closeTime"],openTime=e["openTime"],startTime=e["startTime"],stopTime=e["stopTime"])
+                    self.addElev(elev)
+        except IOError as e:
+            print("dont work")
+
+
+
+
+
+
 
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from building import Building
+from elevator import Elevator
+import data
+if __name__ == '__main__':
+    checkb=Building()
+    checkb.load_json("data\Ex1_input\Ex1_Buildings\B2.json")
