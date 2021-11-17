84353ad83c90b4d645ea750d79922ff4ee926f8b
michaelMatve <michael.matve@gmail.com>
2021-11-17 11:30:19 +0200

    with elev class and build class

---
+++ src/building.py
@@ -0,0 +1,6 @@
+class Building:
+    def __init__(self,minFloor:int,maxFloor:int):
+        self.elevators={}
+        self.minFloor=minFloor
+        self.maxFloor=maxFloor
+
---
+++ src/elevator.py
@@ -0,0 +1,12 @@
+
+class Elevator:
+    def __init__(self,id:int,speed:float,minFloor:int,maxFloor:int,closeTime:float,openTime:float,startTime:float,stopTime:float ):
+        self.id=id
+        self.speed=speed
+        self.minFloor-minFloor
+        self.maxFloor=maxFloor
+        self.closeTime=closeTime
+        self.openTime=openTime
+        self.startTime=startTime
+        self.stopTime=stopTime
+
