70255832760f134da13ff723710fd76fa5071a4a
EylonNaamat <eylonnaamat@gmail.com>
2021-11-22 12:42:08 +0200

    edit callforelev

--- src/callForElev.py
+++ src/callForElev.py
@@ -15,3 +15,10 @@ class CallForElev:
     def __repr__(self):
         return f"name: {self.name}, time: {self.time}, src: {self.src}, dest: {self.dest}, status: {self.status}, elev: {self.elev} \n"
 
+    def state_of_call(self) -> int:
+        state = 0
+        if self.dest - self.src > 0:
+            state = 1       #up
+        if self.dest - self.src < 0:
+            state = -1      # down
+        return state
